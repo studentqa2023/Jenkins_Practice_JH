0e49c8c4306c94d3fd05eed74b05d64688ba823d
studentqa2023 <rezwanislam857@gmail.com>
2023-11-12 00:17:55 -0500

    first commit

---
+++ src/main/java/jenkins_new/PracticeJenkinsCI.java
@@ -0,0 +1,14 @@
+package jenkins_new;
+
+public class PracticeJenkinsCI {
+	
+	public void getCIJenkins() {
+		
+		System.out.println("github to jenkins");
+	}
+ public static void main(String[] args) {
+	
+	 PracticeJenkinsCI obj = new PracticeJenkinsCI();
+	 obj.getCIJenkins();
+}
+}
